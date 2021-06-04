d213fdcb95e79bbcdce72a674ce9c4671b910e50
rotonda <rotondwamulelu@gmail.com>
2021-06-04 15:30:54 +0200

    first commit

---
+++ src/container/aboutMe/aboutMe.js
@@ -0,0 +1,40 @@
+import React from "react";
+import img from "../../assets/Cs-2.jpg";
+import { AboutMe } from "../../components";
+import { DownloadCVButton } from "../../components/buttons";
+import cv from "../../assets/cv/SHABANGU CHARMAINE.pdf";
+import letter from "../../assets/cv/charmaine shabangu.pdf";
+
+export default function AboutMeContainer() {
+  return (
+    <AboutMe>
+      <AboutMe.Container>
+        <AboutMe.Column>
+          <AboutMe.Image src={img} />
+        </AboutMe.Column>
+        <AboutMe.Column>
+          <AboutMe.Heading2>About Me</AboutMe.Heading2>
+          <AboutMe.Heading4>
+            Hello! My Name is charmaine Shabangu.
+          </AboutMe.Heading4>
+          <AboutMe.Text>
+            I am doing industrial engineering which is the course i desire the
+            most because I am passionate about my work and Because I love what I
+            do.
+          </AboutMe.Text>
+          <AboutMe.Text>
+            I have a steady source of motivation that drives me to do my best.
+          </AboutMe.Text>
+        </AboutMe.Column>
+      </AboutMe.Container>
+      <AboutMe.Buttons>
+        <DownloadCVButton href={cv} target="_blank">
+          CV
+        </DownloadCVButton>
+        <DownloadCVButton href={letter} target="_blank">
+          Cover Letter
+        </DownloadCVButton>
+      </AboutMe.Buttons>
+    </AboutMe>
+  );
+}
---
+++ src/container/contact/contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Contact } from "../../components";
+import { Title } from "../../components/header/styles/header";
+import { LocationIcon, MailIcon, PhoneIcon } from "../../icons/icons";
+
+export default function ContactContainer() {
+  return (
+    <Contact>
+      <Contact.Header>
+        <Contact.Title>Contact Me</Contact.Title>
+        <Contact.Text></Contact.Text>
+      </Contact.Header>
+      <Contact.Body>
+        <Contact.Icons>
+          <Contact.IconsItem>
+            <PhoneIcon />
+            <Contact.IconsTitle>Call Me On</Contact.IconsTitle>
+            <Contact.Text>+27 768288769</Contact.Text>
+          </Contact.IconsItem>
+          <Contact.IconsItem>
+            <MailIcon />
+            <Contact.IconsTitle>Email Me At</Contact.IconsTitle>
+            <Contact.Text email>cpshabagu17@gmail.com</Contact.Text>
+          </Contact.IconsItem>
+          <Contact.IconsItem>
+            <LocationIcon />
+            <Contact.IconsTitle>Visit Me</Contact.IconsTitle>
+            <Contact.Text>
+              06 Phoenix street, West Park, Pretoria West, 0183
+            </Contact.Text>
+          </Contact.IconsItem>
+        </Contact.Icons>
+      </Contact.Body>
+    </Contact>
+  );
+}
